2bba150d8a9765433058437fd79b09c6f028af86
samuelmangarodrigues <samuelmanga22222222@outlook.com>
2022-02-23 16:18:37 -0300

    feat:Rotas-Basicas-2 Finalizando Entrega

--- rotas_basicas/app.py
+++ rotas_basicas/app.py
@@ -1,1 +1,28 @@
-# Seu código aqui
+from flask import Flask
+from datetime import datetime
+from os import getenv
+
+
+app=Flask(__name__)
+
+@app.get("/")
+def home():
+    return {'data':'Hello Flask'}
+
+
+@app.get('/date')
+def date():
+    morning=getenv('morning')
+    afternoon=getenv('afternoon')
+    message=''
+    date=datetime.now()
+    date_complete=date.strftime('%d/%m/%Y %H:%M:%S')
+    date_time=date.strftime('%p')
+    if date_time=='PM':
+        message=afternoon
+    else:
+        message=morning
+        return message    
+    return {'current_datetime':f'{date_complete} {date_time}',
+            'message':message
+            }
